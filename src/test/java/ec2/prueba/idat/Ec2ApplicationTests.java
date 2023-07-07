bf0b2da14b5c1485f32a6f9787f8fdf191549ec5
David <davidcastroastudillo20@gmail.com>
2023-07-06 19:44:24 -0500

    camibos

---
+++ src/test/java/ec2/prueba/idat/Ec2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ec2.prueba.idat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
